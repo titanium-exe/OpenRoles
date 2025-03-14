11aff116e713cf24484a083f1a21cfcd58f00706
ekamleen <ekamleenm@gmail.com>
2025-03-13 19:43:04 -0700

    connect database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
 
 const app = express();
 
@@ -18,6 +22,7 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
+  connectDB();
   console.log(`server running at port ${PORT}`);
 });
 
