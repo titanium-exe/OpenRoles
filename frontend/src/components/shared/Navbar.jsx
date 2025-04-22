e64906daacca45186f3dddb23e23c81a9b732e8c
ekamleen <ekamleenm@gmail.com>
2025-04-21 21:05:39 -0700

    frontend started

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white'>
+    <h1 className='text-2xl font-bold'>Open<span className='text-[#0033cc]'>Roles!</span></h1>
+    </div>
+  )
+}
+
+export default Navbar
