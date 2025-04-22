888b5208cde6af186b8d843ec0b840c9a3eaff28
ekamleen <ekamleenm@gmail.com>
2025-04-22 16:38:30 -0700

    navbar started

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { Popover, PopoverContent } from "@radix-ui/react-popover";
+import React from "react";
+import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
-    <div className='bg-white'>
-    <h1 className='text-2xl font-bold'>Open<span className='text-[#0033cc]'>Roles!</span></h1>
-    </div>
-  )
-}
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Open<span className="text-[#0033cc]">Roles!</span>
+          </h1>
+        </div>
 
-export default Navbar
+        <div>
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="outline">Open popover</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <h1>Hello!</h1>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
