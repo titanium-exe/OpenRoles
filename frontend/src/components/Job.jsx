05b843922105f798a9b8a05cffe89564733764d6
ekamleen <ekamleenm@gmail.com>
2025-05-03 18:03:54 -0700

    /job

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./ui/button";
+import { Bookmark } from "lucide-react";
+import { Avatar, AvatarImage } from "./ui/avatar";
 
 const Job = () => {
   return (
-    <div>
+    <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
+      <div className="flex items-center justify-between"><p>2 days ago</p>
+      <Button variant="outline" className="rounded-md" size="icon">
+        <Bookmark />
+      </Button></div>
       
+      <div className="flex items-center gap-2 my-2">
+        {" "}
+        <Button className="p-6" variant="outline" size="icon">
+          <Avatar>
+            <AvatarImage src="" />
+          </Avatar>
+        </Button>
+        <h1>Company</h1>
+        <p>Canada</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Job
+export default Job;
