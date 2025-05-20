915fea3277a8cd519312695d083a5672e49cbb8c
titanium-exe <ekamleenm@gmail.com>
2025-05-20 02:39:07 -0700

    job posted

---
+++ frontend/src/components/admin/PostBanner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PostBanner = () => {
+  return (
+    <div className="mx-auto bg-[#2c5f71] py-5 px-10 text-center border border-teal-300 rounded-md w-fit my-10">
+      <h1 className="text-white text-lg font-semibold">
+        Connect with top tech talent instantly.
+      </h1>
+    </div>
+  );
+};
+
+export default PostBanner;
--- frontend/src/components/admin/PostJobs.jsx
+++ frontend/src/components/admin/PostJobs.jsx
@@ -3,6 +3,14 @@ import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { useState } from 'react'
 import { Button } from '../ui/button'
+import { Loader2 } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
+import PostBanner from './PostBanner'
+import axios from 'axios'
+import { JOB_API_POINT } from '@/utils/constant'
+import toast from 'react-hot-toast'
 
 
 const companyArray= [];
@@ -19,16 +27,53 @@ const PostJobs = () => {
         position: 0,
         companyId: ""
   });
+
+  const { companies } = useSelector(store => store.company);
   const changeEventHandler = (e) => {
     setInput({...input, [e.target.name]:e.target.value});
   };
 
+  const selectChangeHandler = (value) => {
+    const selectedCompany = companies.find((company)=> company.name.toLowerCase() === value);
+    setInput({...input, companyId:selectedCompany._id});
+};
+  const [loading, setLoading]= useState(false);
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+    try {
+
+      setLoading(true);
+      const res = await axios.post(`${JOB_API_POINT}/post`, input, {
+        headers:{
+          'Content-Type':'application/json'
+        },
+        withCredentials:true
+      });
+
+      if(res.data.success){
+        toast.success(res.data.message);
+        navigate("/admin/jobs");
+      }
+      
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log(error);
+    }finally{
+      setLoading(false);
+    }
+  };
+
   return (
+
    <div>
     <Navbar />
+    <PostBanner/>
+    
     <div className='flex items-center justify-center w-screen my-5'>
-
-      <form className='p-8 max-w-4xl border-gray-200 shadow-lg rounded-md'>
+      <form onSubmit = {submitHandler} className='p-8 max-w-4xl border-gray-200 shadow-lg rounded-md'>
             <div className='grid grid-cols-2 gap-4'>
 
             <div>
@@ -111,11 +156,35 @@ const PostJobs = () => {
               className="focus-visible:ring-offset-0 focus-visible:ring-0  my-1"
               />
             </div>
+            {
+                            companies.length > 0 && (
+                                <Select onValueChange={selectChangeHandler}>
+                                    <SelectTrigger className="w-[180px]">
+                                        <SelectValue placeholder="Select a Company" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            {
+                                                companies.map((company) => {
+                                                    return (
+                                                        <SelectItem value={company?.name?.toLowerCase()}>{company.name}</SelectItem>
+                                                    )
+                                                })
+                                            }
+
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
+                            )
+                        }
             
             </div>
-            <Button className="w-full mt-5">
-              Post Job
-            </Button>
+            {
+                        loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Post New Job</Button>
+            }
+            {
+                        companies.length === 0 && <p className='text-xs text-red-600 font-bold text-center my-3'>*Please register a company first, before posting a jobs</p>
+            }
             
       </form> 
     </div>
